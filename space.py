74dddceaeed6d55793dcd926ed32ea9e465e47c5
Michael Soldmann <msoldmann@gmail.com>
2021-02-14 10:22:27 -0600

    update comments

--- space.py
+++ space.py
@@ -1,21 +1,21 @@
 import pandas as pd
 import plotly.express as px
 
-#add json API
+# add API in json
 
 url = 'http://api.open-notify.org/iss-now.json'
 df = pd.read_json(url)
 
-#create latitude and logitude as colums with iss_position
+# create latitude and longitude as columns
 
 df['latitude'] = df.loc['latitude','iss_position']
 df['longitude'] = df.loc['longitude', 'iss_position']
 df.reset_index(inplace=True)
 
-#remove index and message columns as they are unneccessary and now easier to plot
+# remove index and message columns
 df = df.drop(['index', 'message'], axis=1)
 
-# use plotly to plot the latitude and longitude
+# use plotly to plot the latitude and longitude on global map
 
 fig = px.scatter_geo(df, lat='latitude', lon='longitude')
 
