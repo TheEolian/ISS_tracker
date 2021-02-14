ca1aa08731ada2a0259e288a3dd56c65b3dadabf
Michael Soldmann <msoldmann@gmail.com>
2021-02-13 20:53:02 -0600

    latitude and longitude columns

--- space.py
+++ space.py
@@ -5,3 +5,9 @@ import plotly.express as px
 
 url = 'http://api.open-notify.org/iss-now.json'
 df = pd.read_json(url)
+
+#create latitude and logitude as colums with iss_position
+
+df['latitude'] = df.loc['latitude','iss_position']
+df['longitude'] = df.loc['longitude', 'iss_position']
+df.reset_index(inplace=True)
